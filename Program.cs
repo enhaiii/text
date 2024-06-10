c0ec13d44668cd498c463abc75d17a8b3f8184a4
Студент группы IS <is@sielom.local>
2024-06-10 11:15:58 +0500

    1 commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int Sum(int a, int b)
+{
+    return a + b;
+}
+int result = Sum(4, 5);
+Console.WriteLine(result);
+
+int Substraction(int a, int b)
+{
+    return a - b;
+}
+int result2 = Substraction(10, 2);
+Console.WriteLine(result2);
+
+int Div(int a, int b)
+{
+    return a * b;
+}
+int result3 = Div(4, 5);
+Console.WriteLine(result3);
